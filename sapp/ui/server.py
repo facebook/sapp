f8dfb9086fdd99988e113d2ab4958a5bd731426a
Steven Troxler <stroxler@fb.com>
2021-08-06 08:24:46 -0700

    Use the database engine in the server
    Summary:
    In order to use sapp in the most flexible way, we should support
    all the database flags which means allowing both mysql and sqlite -
    the existing server was using the database name but forcing an sqlite
    connection, which meant if we loaded the data to mysql we couldn't
    then use it in the server.
    
    Reviewed By: dkgi
    
    Differential Revision: D30136311
    
    fbshipit-source-id: 8953e46e0a6c28d8c18cfdeb29c5ea792c7f394a

--- sapp/ui/server.py
+++ sapp/ui/server.py
@@ -63,13 +63,8 @@ def start_server(
     source_directory: str,
     editor_schema: Optional[str],
 ) -> None:
-    engine = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL("sqlite", database=database.dbname),
-        echo=False,
-        poolclass=None,
-    )
     session = scoped_session(
-        sessionmaker(bind=engine),
+        sessionmaker(bind=database.engine),
         # pyre-fixme[16]: `flask` has no attribute _app_ctx_stack
         scopefunc=_app_ctx_stack.__ident_func__,
     )
