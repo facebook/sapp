ddccb6b1bed751f50dab44c896d689a30bf1292d
Nathan Maue <nmaue@fb.com>
2021-08-17 09:49:36 -0700

    Clean up TrimTraceGraph
    Summary: No need to keep around an additional reference to the TraceGraph in the summary, we should rely on pipeline steps if they need it.
    
    Reviewed By: williewillus
    
    Differential Revision: D30368432
    
    fbshipit-source-id: c483aa4ba53276888753b0fa54d25c5feeba93fe

--- sapp/pipeline/trim_trace_graph.py
+++ sapp/pipeline/trim_trace_graph.py
@@ -10,14 +10,12 @@ from ..trace_graph import TraceGraph
 from ..trimmed_trace_graph import TrimmedTraceGraph
 from . import PipelineStep, Summary
 
-# pyre-fixme[5]: Global expression must be annotated.
-log = logging.getLogger("sapp")
+log: logging.Logger = logging.getLogger("sapp")
 
 
 class TrimTraceGraph(PipelineStep[TraceGraph, TraceGraph]):
     def run(self, input: TraceGraph, summary: Summary) -> Tuple[TraceGraph, Summary]:
         if summary.get("affected_files") is None:
-            summary["graph"] = input  # used by ranker
             return input, summary
 
         log.info("Trimming graph to affected files.")
@@ -25,6 +23,4 @@ class TrimTraceGraph(PipelineStep[TraceGraph, TraceGraph]):
             summary["affected_files"], summary.get("affected_issues_only", False)
         )
         trimmed_graph.populate_from_trace_graph(input)
-
-        summary["graph"] = trimmed_graph  # used by ranker
         return trimmed_graph, summary
