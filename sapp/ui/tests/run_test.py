ad57b6ed811af9a11c1776b4a68c32f0d01acc37
JeremAIh <esiebomaj@gmail.com>
2021-11-02 12:34:05 -0700

    added tests for run.py in ui folder (#57)
    Summary:
    **Pre-submission checklist**
    - [x] I've ran the following linters locally and fixed lint errors related to the files I modified in this PR
        - [x] `black .`
        - [x] `usort format .`
        - [x] `flake8`
    - [x] I've installed dev dependencies `pip install -r requirements-dev.txt` and completed the following:
        - [x] I've ran tests with `./scripts/run-tests.sh` and made sure all tests are passing
    
    The PR adds test for `sapp/frontend/ui/run.py` and increases the coverage for that file to 100%
    
    Pull Request resolved: https://github.com/facebook/sapp/pull/57
    
    Test Plan:
    to run the tests in test_run.py
    `python -m coverage run -m unittest sapp.ui.tests.run_test && python -m coverage report --show-missing --skip-covered --skip-empty`
    
    ```
    Name                        Stmts   Miss  Cover   Missing
    ---------------------------------------------------------
    sapp/ui/run.py                25      0    100%
    ```
    
    Relevant Issue : [mlh-fellowship/sapp https://github.com/facebook/sapp/issues/11](https://github.com/MLH-Fellowship/sapp/issues/11)
    
    Reviewed By: dkgi
    
    Differential Revision: D31971123
    
    Pulled By: 0xedward
    
    fbshipit-source-id: 1ff290559cbb7b92cb1df5d0f02eae48e6516bf4

---
+++ sapp/ui/tests/run_test.py
@@ -0,0 +1,72 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from unittest import TestCase
+
+from ...db import DB, DBType
+from ...models import (
+    create as create_models,
+)
+from ...tests.fake_object_generator import FakeObjectGenerator
+from ..run import EmptyDeletionError, delete_run, runs, latest
+
+
+class RunTest(TestCase):
+    def setUp(self) -> None:
+        self.db = DB(DBType.MEMORY)
+        create_models(self.db)
+        self.fakes = FakeObjectGenerator()
+
+        if self.shortDescription() == "No setUp":
+            # setUp does not run if the test has docstring "No setUp"
+            return
+
+        run1 = self.fakes.run()
+        run2 = self.fakes.run()
+        run3 = self.fakes.run()
+
+        with self.db.make_session() as session:
+            session.add(run1)
+            session.add(run2)
+            session.add(run3)
+            session.commit()
+
+    def testRuns(self) -> None:
+        with self.db.make_session() as session:
+            allruns = runs(session)
+
+            self.assertEqual(len(allruns), 3)
+            # pyre-ignore[6]: graphene too dynamic.
+            self.assertEqual(int(allruns[0].run_id), 3)
+            # pyre-ignore[6]: graphene too dynamic.
+            self.assertEqual(int(allruns[1].run_id), 2)
+            # pyre-ignore[6]: graphene too dynamic.
+            self.assertEqual(int(allruns[2].run_id), 1)
+
+    def testLatestRun(self) -> None:
+        with self.db.make_session() as session:
+            id = latest(session)
+            self.assertEqual(int(id), 3)
+
+    def testLatestRunWithNoRuns(self) -> None:
+        """No setUp"""
+        with self.db.make_session() as session:
+            id = latest(session)
+            self.assertEqual(str(id), "None")
+
+    def testDeleteRun(self) -> None:
+        with self.db.make_session() as session:
+            delete_run(session, "1")
+            remainingRuns = runs(session)
+            # pyre-ignore[6]: graphene too dynamic.
+            remainingRunsId = [int(r.run_id) for r in remainingRuns]
+            self.assertEqual(len(remainingRuns), 2)
+            self.assertNotIn(1, remainingRunsId)
+
+    def testDeleteNonExistentRun(self) -> None:
+        with self.db.make_session() as session:
+            self.assertRaises(EmptyDeletionError, delete_run, session, 10)
+            allruns = runs(session)
+            self.assertEqual(len(allruns), 3)
