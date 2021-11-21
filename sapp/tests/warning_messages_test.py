d78aad5b5dc2ea5ff61573089c7b1548d41df7eb
Arpan Abhishek <arpanforbusiness@gmail.com>
2021-11-22 01:55:42 +0530

    added test for updation, test for existence and added tempfile module

--- sapp/tests/warning_messages_test.py
+++ sapp/tests/warning_messages_test.py
@@ -7,7 +7,7 @@ from sqlalchemy import Table
 from ..db import DB, DBType
 from ..models import WarningMessage, create as create_model
 from ..warning_messages import update_warning_messages, upsert_entry
-from .fake_object_generator import FakeObjectGenerator
+import tempfile
 
 test_metadata = {
     "codes": {
@@ -25,29 +25,27 @@ class WarningMessagesTest(TestCase):
     def setUp(self) -> None:
         self.db = DB(DBType.MEMORY)
         create_model(self.db)
-        self.fakes = FakeObjectGenerator()
-
-        with open("sample_metadata.json", "w") as outfile:
-            json.dump(test_metadata, outfile)
-
-    def tearDown(self) -> None:
-        pathlib.Path("sample_metadata.json").unlink()
 
     def test_update_warning_messages(self) -> None:
+        temp = tempfile.NamedTemporaryFile(mode="w+")
+        json.dump(test_metadata, temp)
+        temp.flush()
         with self.db.make_session() as session:
-            update_warning_messages(self.db, pathlib.Path("sample_metadata.json"))
+            update_warning_messages(self.db, pathlib.Path(temp.name))
+            code1001 = (
+                session.query(WarningMessage).filter_by(code="1001").one_or_none()
+            )
+            code1002 = (
+                session.query(WarningMessage).filter_by(code="1002").one_or_none()
+            )
+            self.assertIsNotNone(code1001)
             self.assertEqual(
-                session.query(WarningMessage)
-                .filter(WarningMessage.code == "1001")
-                .first()
-                .__dict__["message"],
+                code1001.message,
                 test_metadata["codes"]["1001"],
             )
+            self.assertIsNotNone(code1002)
             self.assertEqual(
-                session.query(WarningMessage)
-                .filter(WarningMessage.code == "1002")
-                .first()
-                .__dict__["message"],
+                code1002.message,
                 test_metadata["codes"]["1002"],
             )
 
@@ -59,9 +57,11 @@ class WarningMessagesTest(TestCase):
         with self.db.make_session() as session:
             with session.connection() as database_connection:
                 for code, message in codes.items():
+                    # Test for insertion
                     upsert_entry(
                         database_connection, warning_messages_table, int(code), message
                     )
+
                 self.assertEqual(
                     database_connection.execute(
                         warning_messages_table.select().where(
@@ -81,4 +81,18 @@ class WarningMessagesTest(TestCase):
                     .first()
                     .message,
                     "Updated warning message from another test 2",
+                )
+                # Test for Updation
+                upsert_entry(
+                    database_connection, warning_messages_table, 2001, "Testing Update"
+                )
+                self.assertEqual(
+                    database_connection.execute(
+                        warning_messages_table.select().where(
+                            warning_messages_table.c.code == "2001"
+                        )
+                    )
+                    .first()
+                    .message,
+                    "Testing Update",
                 )
