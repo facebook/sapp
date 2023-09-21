27e86606be63226428a18f6b14b0515f54233aa3
Alex Blanck <alexblanck@meta.com>
2023-09-21 14:44:17 -0700

    Log timing metric for SAPP processing to `sapp_metrics`
    Reviewed By: fahndrich
    
    Differential Revision: D49079078
    
    fbshipit-source-id: e9f04a03ab354e1a29f65237e09f34c285b2bd0d

--- sapp/pipeline/database_saver.py
+++ sapp/pipeline/database_saver.py
@@ -121,7 +121,6 @@ class DatabaseSaver(PipelineStep[TraceGraph, RunSummary]):
 
                 run_id = self.summary["run"].id.resolved()
                 log.info("Created run: %d", run_id)
-                self.summary["run"] = None  # Invalidate it
 
             self.bulk_saver.save_all(self.database)
 
