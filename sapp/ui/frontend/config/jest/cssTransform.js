467f6926749092ff03625a289016fd17d0b0fa18
Maxime Arthaud <arthaud@meta.com>
2023-11-12 11:52:19 -0800

    Revert D23522664 so we can upgrade create-react-app
    Summary:
    # Problem
    `npm install && npm run build` currently returns errors because of outdated npm packages.
    
    # Solution
    Instead of trying to manually upgrade everything, which would be a huge amount of work, let's try to upgrade "create-react-app", which is the tool we used to bootstrap the UI.
    Unfortunately, we opted out of "create-react-app" by using "npm run eject", which means we wouldn't follow upgrades.
    This diff reverts that change so we can easily upgrade in a future diff.
    This was done with `patch -R` and then manually fixing a few conflicts.
    
    Reviewed By: alambert
    
    Differential Revision: D51232344
    
    fbshipit-source-id: 83ee1bb4eb2ae0f7f2f170b52d074c4c53f3b658

--- sapp/ui/frontend/config/jest/cssTransform.js
+++ sapp/ui/frontend/config/jest/cssTransform.js
@@ -5,17 +5,3 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
-
-// This is a custom Jest transformer turning style imports into empty objects.
-// http://facebook.github.io/jest/docs/en/webpack.html
-
-module.exports = {
-  process() {
-    return 'module.exports = {};';
-  },
-  getCacheKey() {
-    // The output is always the same.
-    return 'cssTransform';
-  },
-};
--- sapp/ui/frontend/config/pnpTs.js
+++ sapp/ui/frontend/config/pnpTs.js
@@ -5,38 +5,3 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-'use strict';
-
-const { resolveModuleName } = require('ts-pnp');
-
-exports.resolveModuleName = (
-  typescript,
-  moduleName,
-  containingFile,
-  compilerOptions,
-  resolutionHost
-) => {
-  return resolveModuleName(
-    moduleName,
-    containingFile,
-    compilerOptions,
-    resolutionHost,
-    typescript.resolveModuleName
-  );
-};
-
-exports.resolveTypeReferenceDirective = (
-  typescript,
-  moduleName,
-  containingFile,
-  compilerOptions,
-  resolutionHost
-) => {
-  return resolveModuleName(
-    moduleName,
-    containingFile,
-    compilerOptions,
-    resolutionHost,
-    typescript.resolveTypeReferenceDirective
-  );
-};
