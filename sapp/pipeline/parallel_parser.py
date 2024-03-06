d8ad4064cfb147a36b428c242ff0d82e89fa0ed4
generatedunixname89002005287564 <generatedunixname89002005287564@meta.com>
2024-03-06 12:59:07 -0800

    Pyre Configurationless migration for] [batch:230/244]
    Reviewed By: connernilsen
    
    Differential Revision: D54560932
    
    fbshipit-source-id: 8d3a15fa19135f26078d6945421b08a89163b791

--- sapp/pipeline/parallel_parser.py
+++ sapp/pipeline/parallel_parser.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 import logging
 from multiprocessing import Pool
 from typing import Iterable, List, Set, Tuple, Type, Union
--- sapp/ui/query_result.py
+++ sapp/ui/query_result.py
@@ -3,6 +3,8 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+# pyre-strict
+
 import datetime
 
 from typing import Any, Dict, FrozenSet, List, NamedTuple, Set, Union
